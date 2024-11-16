44911fad7d6dcdd029eba222f8cac91b05d47f28
NeilRow56 <neilrowland56@gmail.com>
2024-11-16 16:50:07 +0000

    first commit - shadcn and prettier

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        hostname: "static.vecteezy.com",
+      },
+      {
+        hostname: "avatar.vercel.sh",
+        port: "",
+      },
+      {
+        hostname: "utfs.io",
+        port: "",
+      },
+    ],
+  },
+ 
 };
 
 export default nextConfig;
