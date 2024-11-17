f5220963f31de67e1bdbfeafe2205cba8fd82daf
NeilRow56 <neilrowland56@gmail.com>
2024-11-17 12:08:48 +0000

    Added EventCalendat and Announcements components to admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,4 +1,6 @@
+import Announcements from "@/components/dashboard/Announcements";
 import CountChart from "@/components/dashboard/CountChart";
+import EventCalendar from "@/components/dashboard/EventCalendar";
 import UserCard from "@/components/dashboard/UserCard";
 
 const AdminPage = () => {
@@ -18,18 +20,17 @@ const AdminPage = () => {
           <div className="h-[450px] w-full bg-yellow-100">
             <CountChart />
           </div>
-          {/* ATTENDANCE CHART */}
-          {/* <div className="h-[450px] w-full bg-purple-100 lg:w-2/3">
-            Attendance Chart
-          </div> */}
         </div>
         {/* BOTTOM CHART */}
-        <div className="h-[500px] w-full bg-green-50">Announcements</div>
+        <div className="h-[500px] w-full bg-green-50">
+          <Announcements />
+        </div>
       </div>
       {/* RIGHT */}
       <div className="flex w-full flex-col gap-8 lg:w-1/3">
-        <div className="h-[450px] bg-red-50">Event Calendar</div>
-        {/* <div className="h-[450px] bg-purple-200">Announcements</div> */}
+        <div className="h-[450px] bg-red-50">
+          <EventCalendar />
+        </div>
       </div>
     </div>
   );
