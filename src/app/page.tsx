2a7f47a39a6e6270ffe0ab4d08ff70d20f313082
NeilRow56 <neilrowland56@gmail.com>
2024-11-17 03:29:14 +0000

    Updated Home page and creaed ShinyButton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { ShinyButton } from "@/components/shared/ShinyButton";
+
 export default function Home() {
   return (
-    <div className="flex h-screen w-full items-center justify-center p-24">
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-10 bg-zinc-100 p-24">
       <h1 className="text-5xl font-bold">Home Page</h1>
+      <div className="w-full max-w-80">
+        <ShinyButton
+          href="/sign-up"
+          className="relative z-10 h-14 w-full text-blue-500 shadow-lg transition-shadow duration-300 hover:shadow-xl"
+        >
+          Start For Free Today
+        </ShinyButton>
+      </div>
     </div>
   );
 }
